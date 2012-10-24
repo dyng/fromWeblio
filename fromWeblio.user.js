6c777949b81f97b16c964d0520d58ae8e9a85a3b
tei-you <tei-you@kayac.com>
2012-10-24 15:08:38 +0900

    init commit

---
+++ fromWeblio.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name        fromWeblio
+// @namespace   towords.com
+// @include     http://www.towords.com/
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js
+// @author      dingye
+// @version     1
+// ==/UserScript==
+function fromWeblio() {
+    if (fw_mutex == 1){
+        fw_mutex = 0;
+        var word = $('#wordInfo div:eq(0) span').text();
+
+        GM_xmlhttpRequest({
+            url: "http://ejje.weblio.jp/content/" + word,
+            method: 'GET',
+            onload:function(response){
+                $("#wordInfo div:eq(2)").html($(response.responseText).find("div.Kejje").text());
+                fw_mutex = 1;
+            }
+        });
+    }
+}
+
+function main() {
+    setInterval(function(){
+        if($("#answerDiv").is(":visible")){
+            if(fw_show_once == 1){
+                fw_show_once = 0;
+                fromWeblio();
+            }
+        }else{
+            fw_show_once = 1;
+        }
+    }, 100);
+}
+
+var fw_mutex = 1;
+var fw_show_once = 1;
+main();
